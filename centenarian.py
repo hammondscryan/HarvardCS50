0dbca6908580fa1eab19b04bf80293b477cce123
hammondscryan <hammonds.c.ryan@gmail.com>
2023-05-27 22:53:57 -0500

    corrected __init__ misspelling in centenarian.py

--- centenarian.py
+++ centenarian.py
@@ -1,32 +1,34 @@
 import datetime
 
+
 def main():
-    new_User = Person()
-    #new_User.name = new_User.assign_name()
-    #new_User.age = new_User.assign_age()
+    new_user = User()
+    new_user.name = new_user.assign_name()
+    new_user.age = new_user.assign_age()
 
     current_year = datetime.date.today().year
-    new_User_100th = new_User.calc_100th(current_year)
-    if new_User_100th > current_year:
-        print(f"{new_User.name}, your 100th birthday will be in {new_User_100th}!")
-    elif new_User_100th == current_year:
-        print(f"Have a happy 100th year, {new_User.name}!")
+    new_user_100th = new_user.calc_100th(current_year)
+    if new_user_100th > current_year:
+        print(f"{new_user.name}, your 100th birthday will be in {new_user_100th}!")
+    elif new_user_100th == current_year:
+        print(f"Have a happy 100th year, {new_user.name}!")
     else:
-        print(f"{new_User.name}, your 100th birthday was in {new_User_100th}!")
+        print(f"{new_user.name}, your 100th birthday was in {new_user_100th}!")
 
-class Person:
-    name = "Stranger"
-    age = 0
-    def __int__(self, name: str ="Stranger",age: int =0):
+
+class User:
+    def __init__(self, name: str ="Stranger", age: int =0):
         self.name = name
         self.age = age
+
     def assign_name(self) -> str:
         return input("Please enter your name:")
+
     def assign_age(self) -> int:
         acceptable_age = False
         test_age = input("Please enter your age in years:")
-        while (not acceptable_age):
-            if (test_age.isnumeric() and int(test_age) >= 0):
+        while not acceptable_age:
+            if test_age.isnumeric() and int(test_age) >= 0:
                 acceptable_age = True
             else:
                 print("Your age must a positive whole number of years.")
@@ -34,9 +36,8 @@ class Person:
         return int(test_age)
 
     def calc_100th(self, current_year: int) -> int:
-        return (current_year + (100-self.age))
+        return current_year + (100-self.age)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
