63f7d842eb04486f3622d0c4b60253f294ba784d
hammondscryan <hammonds.c.ryan@gmail.com>
2023-05-29 00:09:04 -0500

    started logic_with_lists.py

--- logic_with_lists.py
+++ logic_with_lists.py
@@ -1,6 +1,8 @@
 def main():
+    print(f"This is {__name__}.")
     intro()
-    factors_list = create_factors_list()
+    new_set = define()
+    '''
     if factors_list is None:
         print("\n<<<Closing factorization program>>>")
     else:
@@ -18,10 +20,11 @@ def main():
                 case 'q':
                     print("<<<Closing factorization program>>>")
 
-
-def create_factors_list() -> list:
-    factors_list = []
-    num = input("\nEnter a positive integer to factorize (or q to quit): ").strip()
+'''
+def create_set() -> list:
+    new_set = []
+'''
+    new_set = input("\nEnter a positive integer to factorize (or q to quit): ").strip()
     while num != 'q':
         while not str.isnumeric(num) or int(num) < 1 or num == 'q':
             print("\nYou must enter a positive integer (i.e., whole number greater than 0).")
@@ -35,30 +38,46 @@ def create_factors_list() -> list:
         else:
             break
     return None
-
+'''
 
 
 def intro():
-    print("\n\n\nThis program provides the factors for a given positive integer.")
-    print("The factors of a positive integer are those positive integers that evenly divide it (remainder of 0).")
+    print("\n\n\nThis program performs basic set logic.")
+    print("It is assumed that every set contains real numbers (duplicates will be deleted).")
 
 
 def valid_menu_choice(choice) -> bool:
-    if choice == 'p' or choice == 'n' or choice == 'q':
+    if choice == 'p'|'n'|'d'|'i'|'u'|'M'|'m'|'q':
         return True
     else:
         return False
 
 
-def main_menu_handler(factors_list: list) -> str:
+def main_menu_handler(new_set: list) -> str:
     menu_choice = ''
     while not valid_menu_choice(menu_choice):
         print("\n<<<MAKE A SELECTION>>>")
-        print(f"(p) Print the factors of {factors_list[len(factors_list)-1]}.")
-        print("(n) Calculate the factors of a new positive integer.")
+        print(f"(p) Print a set.")
+        print("(n) Add a new set.")
+        print("(d) Delete a set.")
+        print("(i) Give the intersection of defined sets.")
+        print("(u) Give the union of defined sets.")
+        print("(M) Calculate the maximum of a given set.")
+        print("(m) Calculate the minimum of a given set.")
         print("(q) End program.")
         menu_choice = input("SELECTION: ").strip()
     return menu_choice
+
+class Set:
+    def __init__(self, contents: list =[]):
+        self.contents = contents
+
+    def define(self):
+        #prompt to ingest a series of real numbers
+        #check to ensure that these numbers are actually real numbers and not alpha
+        #handle errors
+        #assign set to class attrib AND return Bool to indicate success/failure
+
 
 
 if __name__ == "__main__":
