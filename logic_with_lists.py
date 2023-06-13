e26a5c3df13223d3fcf8053409561df71a10c554
hammondscryan <hammonds.c.ryan@gmail.com>
2023-06-12 20:00:22 -0500

    So much......

--- logic_with_lists.py
+++ logic_with_lists.py
@@ -1,24 +1,37 @@
 
 
 class Set:
-    def __init__(self, name: str = str(), element_list: list = []):
+    def __init__(self, name: str = str(), element_list: list = list()):
         self.name = name
         self.element_list = element_list
-        self.multiplicity_table = {}
+        self.multiplicity_table = dict()
         for element in element_list:
             if str(element) not in self.multiplicity_table:
                 self.multiplicity_table[str(element)] = 1
             else:
                 self.multiplicity_table[str(element)] += 1
 
-    def update_set(self, element_list: list):
-        self.element_list = element_list
-        self.multiplicity_table.clear()
-        for element in self.element_list:
-            if str(element) not in self.multiplicity_table:
-                self.multiplicity_table[str(element)] = 1
-            else:
-                self.multiplicity_table[str(element)] += 1
+    def update_set(self, element_list: list = list(),
+                   multiplicity_table: dict = dict(),
+                   name: str = str(),
+                   option: str = 'e'):
+        match option:
+            case 'e':
+                self.element_list = element_list
+                self.multiplicity_table.clear()
+                for element in self.element_list:
+                    if str(element) not in self.multiplicity_table:
+                        self.multiplicity_table[str(element)] = 1
+                    else:
+                        self.multiplicity_table[str(element)] += 1
+            case 'm':
+                self.multiplicity_table = multiplicity_table
+                self.element_list.clear()
+                for element in self.multiplicity_table:
+                    for _ in range(multiplicity_table[element]):
+                        self.element_list.append(float(element))
+            case 'n':
+                self.name = name
 
     def add_element(self, new_element: float):
         self.element_list.append(new_element)
@@ -51,7 +64,7 @@ class Set:
 
 def main():
 
-    all_sets = {}
+    all_sets = dict()
     print_intro()
     while True:
         menu_choice = main_menu_handler()
@@ -84,7 +97,7 @@ def print_intro():
 
 
 def main_menu_handler() -> str:
-    menu_choice = ''
+    menu_choice = str()
     while not valid_menu_choice(menu_choice):
         print("\n<<<MAKE A SELECTION>>>")
         print("(p) Print a set.")
@@ -135,11 +148,11 @@ def print_set(all_sets: dict):
 
 
 def multiplicity(all_sets: dict):
-    results = {}
+    results = dict()
     while True:
         try:
             element = input("Which element are you finding the multiplicity of?\n"
-                           "Type < to return to the menu.").strip()
+                            "Type < to return to the menu.").strip()
             if element == '<':
                 print("Returning to the main menu.".upper())
                 break
@@ -159,7 +172,8 @@ def multiplicity(all_sets: dict):
                         results[a_set] = 0
                     else:
                         results[a_set] = all_sets[a_set].multiplicity_table[str(element)]
-                print(results)
+                print(f"The multiplicity of {element} in all sets is:\n",
+                      f"{results}")
                 return
             else:
                 sets_to_check = [x.strip() for x in choice.split(',')]
@@ -170,7 +184,8 @@ def multiplicity(all_sets: dict):
                         results[a_set] = 0
                     else:
                         results[a_set] = all_sets[a_set].multiplicity_table[str(element)]
-                print(results)
+                print(f"The multiplicity of of {element} in {sets_to_check} is:\n"
+                      f"{results}")
                 return
             break
 
@@ -228,7 +243,7 @@ def delete_set(all_sets: dict):
                 for a_set in sets_to_check:
                     assert a_set in all_sets
             except AssertionError:
-                error_list = []
+                error_list = list()
                 for a_set in sets_to_check:
                     if a_set not in all_sets:
                         error_list.append(a_set)
@@ -241,13 +256,94 @@ def delete_set(all_sets: dict):
 
 
 def intersection(all_sets: dict):
-    print("intersection()")
-    return True
+    ix_set = dict()
+    results = dict()
+    while True:
+        choice = input("Which sets would you like to intersect (separated by commas)?\n"
+                       "Type @ to intersect all sets or < to return to the menu.").strip()
+        if choice == '<':
+            print("Returning to the main menu.".upper())
+            break
+        elif choice == '@':
+            for i, a_set in enumerate(all_sets):
+                if i == 0:
+                    ix_set = all_sets[a_set].multiplicity_table.copy()
+                else:
+                    for element in ix_set:
+                        if element in all_sets[a_set].multiplicity_table:
+                            ix_set[element] = min(ix_set[element], all_sets[a_set].multiplicity_table[element])
+                        else:
+                            ix_set[element] = 0
+            for element in ix_set:
+                if ix_set[element] != 0:
+                    results[element] = ix_set[element]
+            if len(results) == 0:
+                print(f"These intersection of all sets is empty.")
+            else:
+                print(f"The intersection of all sets is {parse_multiplicity_table(results)}.")
+        else:
+            sets_to_check = [x.strip() for x in choice.split(',')]
+            try:
+                for i, a_set in enumerate(sets_to_check):
+                    if i == 0:
+                        ix_set = all_sets[a_set].multiplicity_table
+                    else:
+                        for element in ix_set:
+                            if element in all_sets[a_set].multiplicity_table:
+                                ix_set[element] = min(ix_set[element], all_sets[a_set].multiplicity_table[element])
+                            else:
+                                ix_set[element] = 0
+                for element in ix_set:
+                    if ix_set[element] != 0:
+                        results[element] = ix_set[element]
+                if len(results) == 0:
+                    print(f"These intersection of the following sets is empty.\n"
+                          f"{sets_to_check}")
+                else:
+                    print(f"The intersection of {sets_to_check} is:\n"
+                          f"{parse_multiplicity_table(results)}")
+            except KeyError:
+                error_list = list()
+                for a_set in sets_to_check:
+                    if a_set not in all_sets:
+                        error_list.append(a_set)
+                print(f"The following sets do not exist: {error_list}.".upper())
+        break
 
 
 def union(all_sets: dict):
-    print("union()")
-    return True
+    union_set = list()
+    while True:
+        choice = input("Which sets would you like to union (separated by commas)?\n"
+                       "Type @ to union all sets or < to return to the menu.").strip()
+        if choice == '<':
+            print("Returning to the main menu.".upper())
+            break
+        elif choice == '@':
+            for a_set in all_sets:
+                union_set += all_sets[a_set].element_list
+            if len(union_set) == 0:
+                print(f"The union of all sets is empty.")
+            else:
+                print(f"The union of all sets is {union_set}.")
+        else:
+            sets_to_check = [x.strip() for x in choice.split(',')]
+            try:
+                for a_set in sets_to_check:
+                    union_set += all_sets[a_set].element_list
+                if len(union_set) == 0:
+                    print("The union of the following sets is empty:"
+                          f"{sets_to_check}")
+                print(f"The union of {sets_to_check} is:\n"
+                      f"{union_set}")
+                return
+            except KeyError:
+                error_list = list()
+                for a_set in sets_to_check:
+                    if a_set not in all_sets:
+                        error_list.append(a_set)
+                print(f"The following sets do not exist: {error_list}.".upper())
+        break
 
 
 def max_calc(all_sets: dict):
@@ -258,21 +354,21 @@ def max_calc(all_sets: dict):
             print("Returning to the main menu.".upper())
             break
         elif choice == '@':
-            max_elements = []
+            max_elements = list()
             for a_set in all_sets:
                 max_elements.append(all_sets[a_set].maximum())
             print(f"{max(max_elements)} is the maximum value across all sets.\n")
         else:
             sets_to_check = [x.strip() for x in choice.split(',')]
             try:
-                max_elements = []
+                max_elements = list()
                 for a_set in sets_to_check:
                     max_elements.append(all_sets[a_set].maximum())
                 print(f"{max(max_elements)} is the maximum value of the following sets:\n"
                       f"{sets_to_check}")
                 return
             except KeyError:
-                error_list = []
+                error_list = list()
                 for a_set in sets_to_check:
                     if a_set not in all_sets:
                         error_list.append(a_set)
@@ -288,26 +384,44 @@ def min_calc(all_sets: dict):
             print("Returning to the main menu.".upper())
             break
         elif choice == '@':
-            min_elements = []
+            min_elements = list()
             for a_set in all_sets:
                 min_elements.append(all_sets[a_set].minimum())
             print(f"{min(min_elements)} is the minimum value across all sets.\n")
         else:
             sets_to_check = [x.strip() for x in choice.split(',')]
             try:
-                min_elements = []
+                min_elements = list()
                 for a_set in sets_to_check:
                     min_elements.append(all_sets[a_set].minimum())
                 print(f"{min(min_elements)} is the minimum value of the following sets:\n"
                       f"{sets_to_check}")
                 return
             except KeyError:
-                error_list = []
+                error_list = list()
                 for a_set in sets_to_check:
                     if a_set not in all_sets:
                         error_list.append(a_set)
                 print(f"The following sets do not exist: {error_list}.".upper())
         break
+
+
+def parse_multiplicity_table(m_table: dict) -> list:
+    e_list = list()
+    for element in m_table:
+        for _ in range(m_table[element]):
+            e_list.append(float(element))
+    return e_list
+
+
+def create_multiplicity_table(e_list: list) -> dict:
+    m_table = dict()
+    for element in e_list:
+        if str(element) not in m_table:
+            m_table[str(element)] = 1
+        else:
+            m_table[str(element)] += 1
+    return m_table
 
 
 if __name__ == "__main__":
